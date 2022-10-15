d5873814f54bea383db9d79c55e99103357b86c8
Joshua Bender <36bend@gmail.com>
2022-10-15 16:12:36 -0400

    swap working

--- frontend/components/Swap.tsx
+++ frontend/components/Swap.tsx
@@ -1,10 +1,27 @@
+import { FetchBalanceResult } from "@wagmi/core";
 import { BigNumber, ethers } from "ethers";
 import * as React from "react";
-import { useAccount, useBalance, useContractRead } from "wagmi";
+import {
+  useAccount,
+  useBalance,
+  useContractRead,
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import useDebounce from "../utils/debounce";
+
+const getBalance = (address: string): FetchBalanceResult | undefined => {
+  const { data } = useBalance({
+    addressOrName: address,
+  });
+  return data;
+};
 
 export default function Swap() {
   const { address, isConnected } = useAccount();
   const [swap, setSwapAmount] = React.useState("0");
+  const debouncedSwap = useDebounce(swap, 500);
 
   let price: number | BigNumber = 0;
   if (!swap) {
@@ -12,22 +29,43 @@ export default function Swap() {
   } else {
     price = ethers.utils.parseEther(swap);
   }
+  const balanceEth = getBalance(String(address));
 
-  const { data } = useBalance({
-    addressOrName: address,
-  });
+  // const { data } = useBalance({
+  //   addressOrName: address,
+  // });
 
   const balance = useBalance({
     addressOrName: address,
     token: "0x1B66F72E2aD41Fab10EFa591A224f1f52C44D855",
   });
-  const ethBalance = Number(data?.formatted);
+  const ethBalance = Number(balanceEth?.formatted);
   const ethAmount = Number(Number(price) / 10e17);
-  console.log("balance", ethBalance);
-  console.log("amount", ethAmount);
-  //console.log("balance", Number(balance.data?.formatted));
   const hasEnoughEth = ethBalance > ethAmount;
-  console.log(hasEnoughEth);
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: "0x904Cdbc42a3ECDA75A8547D785914a4862Aa42b9",
+    contractInterface: [
+      {
+        inputs: [],
+        name: "swapETHForERC20",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+    ],
+    functionName: "swapETHForERC20",
+    enabled: Boolean(debouncedSwap),
+    overrides: {
+      from: address,
+      value: ethers.utils.parseEther(swap),
+    },
+  });
+  const { data, write } = useContractWrite(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
 
   const contractRead = useContractRead({
     addressOrName: "0x904Cdbc42a3ECDA75A8547D785914a4862Aa42b9",
@@ -79,12 +117,34 @@ export default function Swap() {
       </form>
       {!isConnected ? (
         <button disabled={true}>Connect Wallet</button>
-      ) : isNaN(parseInt(swap)) || parseInt(swap) === 0 ? (
+      ) : isNaN(parseFloat(swap)) || parseFloat(swap) === 0 ? (
         <button disabled={true}>Enter an amount</button>
       ) : hasEnoughEth ? (
-        <button>Swap</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            write?.();
+          }}
+          disabled={!write || isLoading}
+        >
+          {isLoading ? "Swapping..." : "Swap"}
+        </button>
       ) : (
         <button disabled={true}>Insufficient ETH Balance</button>
+      )}
+      {isSuccess && (
+        <div>
+          Swap submitted successfully!
+          <div>
+            <a
+              href={`https://goerli.etherscan.io/tx/${data?.hash}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Etherscan
+            </a>
+          </div>
+        </div>
       )}
     </div>
   );
